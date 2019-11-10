a5619b9cfe2f98006968fee20d2fe2ddfb657c9e
Edison Neto <ednetoali@gmail.com>
2019-11-10 16:53:58 -0300

    Add docker-compose files

---
+++ cassandra/cassandra-init.sh
@@ -0,0 +1,9 @@
+CQL="DROP keyspace keyspaceName;
+CREATE KEYSPACE keyspaceName WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'} AND durable_writes = true;"
+
+until echo $CQL | cqlsh; do
+    echo "cqlsh: Cassandra is unavailable to initialize - will retry later"
+    sleep 2
+done &
+
+exec /docker-entrypoint.sh "$@"
